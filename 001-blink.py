3421384a74d2ea5ddbbc66b663feb470b574f47d
Jer-El <jerry@jerrywaller.org>
2018-06-12 16:39:52 -0400

    Create 001-blink.py
    Commit #1

---
+++ 001-blink.py
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# credit for this code goes to https://thepihut.com/blogs/raspberry-pi-tutorials/27968772-turning-on-an-led-with-your-raspberry-pis-gpio-pins
+# from whom I borrowed liberally. Thank you.
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(18,GPIO.OUT)
+print "LED on"
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(18,GPIO.LOW)
